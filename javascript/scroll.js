e7008b01122034b7143179c97a920df50d698edc
scott1702 <scott@silverstripe.com>
2015-09-02 18:48:58 +1200

    fix vh bug in ios 7

--- javascript/scroll.js
+++ javascript/scroll.js
@@ -1,28 +1,24 @@
-window.smoothScroll = function(target) {
-    var scrollContainer = target;
-    do { //find scroll container
-        scrollContainer = scrollContainer.parentNode;
-        if (!scrollContainer) return;
-        scrollContainer.scrollTop += 1;
-    } while (scrollContainer.scrollTop == 0);
-
-    var targetY = 0;
-    do { //find the top of target relatively to the container
-        if (target == scrollContainer) break;
-        targetY += target.offsetTop;
-    } while (target = target.offsetParent);
-
-    scroll = function(c, a, b, i) {
-        i++; if (i > 30) return;
-        c.scrollTop = a + (b - a) / 30 * i;
-        setTimeout(function(){ scroll(c, a, b, i); }, 15);
-    }
-    // start scrolling
-    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
-}
-
 $(document).ready(function () {
+    resizeDiv();
+
+    $('nav a').click(function(){
+        $('html, body').animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
+
     $('.phone').click(function () {
         $(this).closest('ul').toggleClass('phone-active');
     });
-}); 
+});
+
+window.onresize = function(event) {
+    resizeDiv();
+}
+
+function resizeDiv() {
+    vpw = $(window).width();
+    vph = $(window).height();
+    $('header, .final').css({'height': vph + 'px'});
+}
